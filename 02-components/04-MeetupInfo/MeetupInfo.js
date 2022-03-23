011b862349d67cc4eec72be02ecf0d72b0835624
avroma <avromanov17@gmail.com>
2022-03-23 21:48:21 +0300

    Add meetup info component

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -3,19 +3,47 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props: {
+    organizer: {
+      type: String,
+      required: true,
+    },
+    place: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  computed: {
+    isoStringDate() {
+      const date = new Date(this.date);
+      const dateOnlyLength = 10;
+      return date.toISOString().substr(0, dateOnlyLength);
+    },
+    localeDate() {
+      const date = new Date(this.date);
+      const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+      return date.toLocaleDateString('en-US', dateOptions);
+    },
+  },
+
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="isoStringDate">{{ localeDate }}</time>
       </li>
     </ul>`,
 });
