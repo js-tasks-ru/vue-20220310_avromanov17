069050e75a7fd084ce55869c1c5ce2b06378793b
avroma <avromanov17@gmail.com>
2022-11-01 21:53:42 +0300

    Update PromiseWrapper component

--- 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
+++ 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
@@ -1,8 +1,16 @@
 <template>
-  <!-- -->
+  <slot v-if="state === states.fulfilled" name="fulfilled" :result="result" />
+  <slot v-else-if="state === states.rejected" name="rejected" :error="error" />
+  <slot v-else name="pending" />
 </template>
 
 <script>
+const states = Object.freeze({
+  pending: 'pending',
+  fulfilled: 'fulfilled',
+  rejected: 'rejected',
+});
+
 export default {
   name: 'PromiseWrapper',
 
@@ -10,6 +18,40 @@ export default {
     promise: {
       type: Promise,
       required: true,
+    },
+  },
+
+  data() {
+    return {
+      states,
+      state: states.pending,
+      result: null,
+      error: null,
+    };
+  },
+
+  watch: {
+    promise: {
+      immediate: true,
+      async handler(newPromise) {
+        this.state = this.states.pending;
+        await newPromise
+          .then((result) => {
+            this.handleResponse(states.fulfilled, result);
+          })
+          .catch((error) => {
+            this.handleResponse(states.rejected, error);
+          });
+      },
+    },
+  },
+
+  methods: {
+    handleResponse(state, result) {
+      const isFulfilled = state === this.states.fulfilled;
+      this.result = isFulfilled ? result : null;
+      this.error = isFulfilled ? null : result;
+      this.state = state;
     },
   },
 };
